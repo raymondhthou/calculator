c19a84277451bc1e0c8f4ea08c28a0969894ed72
hour88 <hour88@Xubuntu.myguest.virtualbox.org>
2024-02-12 12:19:52 +0000

    event listeners for number/decimal input, math operators and clear display values

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const displayValueOne = document.querySelector('.display_1');
 const displayValueTwo = document.querySelector('.display_2');
-const numbersInput  = document.querySelectorAll('.digit');
+const numbersInput = document.querySelectorAll('.digit');
 const operationInput = document.querySelectorAll('.operator');
 const equalResult = document.querySelector('.equal');
 const deleteAll = document.querySelector('.clear_all');
@@ -10,4 +10,46 @@ let operandA = '';
 let operandB = '';
 let result = null;
 let decimalPoint = false;
+let previousOperation = '';
 
+
+
+// Calculator numbers and decimal input
+numbersInput.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if (e.target.innerText === '.' && !decimalPoint) {
+            decimalPoint = true;
+        } else if (e.target.innerText === '.' && decimalPoint) {
+            return;
+        }
+        operandB += e.target.innerText;
+        displayValueTwo.innerText = operandB;
+    })
+});
+
+
+
+// Calculator operation input for mathematical operators
+operationInput.forEach(operation => {
+    operation.addEventListener('click', (e) => {
+        if (!operandB) return;
+        decimalPoint = false;
+        const operationValue = e.target.innerText;
+        if(operandA && operandB && previousOperation) {
+            calculateOperation();
+        } else {
+            result = parseFloat(operandB);
+        }
+        clearVariable(operationValue);
+    })
+});
+
+
+
+// Function will clear input variable values in display_2 to display_1 calculator
+function clearVariable(operator) {
+    operandA += operandB + '' + operator + '';
+    displayValueOne.innerText = operandA;
+    displayValueTwo.innerText = '';
+    operandB = '';
+}
